86e6a1fb4542908b7ba2dfd0fd2475f49697c251
Oleksandr Andrieiev <kuricyak@gmail.com>
2018-09-07 18:39:53 +0300

    added uncommited changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import MovieBrowser from './containers/MovieBrowser'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+     <MovieBrowser />
+    );
+  }
+}
+
+export default App;
